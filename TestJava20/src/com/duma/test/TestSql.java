fe79a34d39b2f424cd5e8165b1ff13d56684ebaf
martin <saxery@gmail.com>
2016-03-28 17:28:58 +0800

    test ingo

---
+++ TestJava20/src/com/duma/test/TestSql.java
@@ -0,0 +1,32 @@
+package com.duma.test;
+
+import java.sql.*;
+
+/**
+ * Created by martin on 2016/3/27.
+ */
+public class TestSql {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = DriverManager.getConnection("url", "username", "password");
+            stmt = conn.createStatement();
+            String sqlTxt = "select * from driver";
+            ResultSet rs = stmt.executeQuery(sqlTxt);
+            int i = 0;
+            int j = 1;
+            int k = i + j;
+            System.out.println(k);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) stmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException ignore) {}
+        }
+    }
+}
